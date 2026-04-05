c278077c0a22999df411859f0f51cdb2c9b90cb2
Artem Chupylko <achupylko@gmail.com>
2026-04-05 10:49:30 +0000

    feat: add styles for search bar component

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,12 +1,17 @@
+import toast from 'react-hot-toast';
+
+import css from './SearchBar.module.css';
+
 interface SearchBarProps {
   onSubmit: (value: string) => void;
 }
 
 export default function SearchBar({ onSubmit }: SearchBarProps) {
-  const handlerSubmit = (formData: FormData) => {
+  const handleSubmit = (formData: FormData) => {
     const query = formData.get('query') as string;
 
     if (query.trim() === '') {
+      toast.error('Please enter your search query.');
       return;
     }
 
@@ -15,24 +20,28 @@ export default function SearchBar({ onSubmit }: SearchBarProps) {
 
   return (
     <>
-      <header>
-        <div>
+      <header className={css.header}>
+        <div className={css.container}>
           <a
+            className={css.link}
             href="https://www.themoviedb.org/"
             target="_blank"
             rel="noopener noreferrer"
           >
             Powered by TMDB
           </a>
-          <form action={handlerSubmit}>
+          <form className={css.form} action={handleSubmit}>
             <input
+              className={css.input}
               type="text"
               name="query"
               autoComplete="off"
               placeholder="Search movies..."
               autoFocus
             />
-            <button type="submit">Search</button>
+            <button className={css.button} type="submit">
+              Search
+            </button>
           </form>
         </div>
       </header>
