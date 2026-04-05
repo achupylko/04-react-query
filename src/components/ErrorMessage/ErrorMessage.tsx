13a3b1b5d446b8b66b0d70f662eabf9bb2bccf5f
Artem Chupylko <achupylko@gmail.com>
2026-04-05 12:46:46 +0000

    feat: add styles for ErrorMessage component

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -1,3 +1,5 @@
+import css from './ErrorMessage.module.css';
+
 export default function ErrorMessage() {
-  return <p>There was an error, please try again...</p>;
+  return <p className={css.text}>There was an error, please try again...</p>;
 }
