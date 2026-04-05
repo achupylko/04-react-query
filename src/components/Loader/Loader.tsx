148b35c0c278e89cfb6bfae31e84c963f03fc778
Artem Chupylko <achupylko@gmail.com>
2026-04-05 12:36:29 +0000

    feat: add styles for Loader component

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,3 +1,5 @@
+import css from './Loader.module.css';
+
 export default function Loader() {
-  return <p>Loading movies, please wait...</p>;
+  return <p className={css.text}>Loading movies, please wait...</p>;
 }
